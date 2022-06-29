aed88daf7884cd9ed1814431a98dafd4de29c9e6
supercll <1033581609@qq.com>
2022-06-29 21:32:55 +0800

    feat: jsx归档

---
+++ archive/01-jsx/src/index.js
@@ -0,0 +1,14 @@
+import React from './react'
+import ReactDOM from './react-dom'
+let element1 = (
+  <div className="title" style={{ color: 'red' }}>
+    <h2>hello</h2>world
+  </div>
+)
+console.log(element1)
+console.log(JSON.stringify(element1, null, 2))
+debugger
+ReactDOM.render(element1, document.getElementById('root'))
+// react核心设计React16
+// React17 fiber
+// React18 优先级调度，并发执行
---
+++ archive/01-jsx/src/react.js
@@ -0,0 +1,34 @@
+import { wrapToVdom } from './utils'
+import { REACT_ELEMENT } from './constants'
+function createElement(type, config, children) {
+  let ref //是后面用来获取真实DOM元素的
+  let key //用来实现DOM-DIFF，高效快速进行DOM比较
+  if (config) {
+    delete config.__source
+    delete config.__self
+    ref = config.ref
+    delete config.ref
+    key = config.key
+    delete config.key
+  }
+  let props = { ...config }
+  if (arguments.length > 3) {
+    //如果有多个儿子，那此处就是一个数组
+    props.children = Array.prototype.slice.call(arguments, 2).map(wrapToVdom)
+  } else {
+    //如果只有一个儿子，children是对象或者说字符串，如果没有儿子，就是undefined
+    //children不一定是数组
+    props.children = wrapToVdom(children)
+  }
+  return {
+    $$typeof: REACT_ELEMENT, //表示这是一个虚拟DOM，也就是说这是一个React元素
+    type, //虚拟DOM元素的类型
+    ref,
+    key,
+    props, //这是属性对象 id  className style ....
+  }
+}
+const React = {
+  createElement,
+}
+export default React
