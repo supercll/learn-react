660fd9b9652dc8e9c4cd8a2d7946dc9b61db9dc9
supercll <1033581609@qq.com>
2022-07-13 21:54:24 +0800

    feat: useImperativeHandle与forwardRef

--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-const CounterContext = React.createContext()
-function reducer(state = { number: 0 }, action) {
-  switch (action.type) {
-    case 'ADD':
-      return { number: state.number + 1 }
-    case 'MINUS':
-      return { number: state.number - 1 }
-    default:
-      return state
-  }
+import React from './react'
+import ReactDOM from './react-dom'
+
+function Child(props, ref) {
+  const inputRef = React.useRef()
+  React.useImperativeHandle(ref, () => ({
+    focus() {
+      inputRef.current.focus()
+    },
+  }))
+  return <input type="text" ref={inputRef} />
 }
-function Counter() {
-  const { state, dispatch } = React.useContext(CounterContext)
+const ForwardedChild = React.forwardRef(Child)
+function Parent() {
+  const inputRef = React.useRef()
+  const getFocus = () => {
+    inputRef.current.focus()
+    inputRef.current.remove()
+  }
   return (
     <div>
-      <p>{state.number}</p>
-      <button onClick={() => dispatch({ type: 'ADD' })}>+</button>
-      <button onClick={() => dispatch({ type: 'MINUS' })}>+</button>
+      <ForwardedChild ref={inputRef} />
+      <button onClick={getFocus}>获得焦点</button>
     </div>
   )
 }
-function App() {
-  const [state, dispatch] = React.useReducer(reducer, { number: 0 })
-  return (
-    <CounterContext.Provider value={{ state, dispatch }}>
-      <Counter />
-    </CounterContext.Provider>
-  )
-}
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Parent />, document.getElementById('root'))
